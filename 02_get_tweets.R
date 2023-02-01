7aa9eaff7e90f440cc77e34c83d00916ef6122f1
cjbarrie <chrisjbarrie@googlemail.com>
2023-02-01 12:22:33 +0000

    Add example twitter collection loop

---
+++ 02_get_tweets.R
@@ -0,0 +1,53 @@
+library(academictwitteR) # see: https://github.com/cjbarrie/academictwitteR
+library(readr)
+library(dplyr)
+options(scipen = 999)
+
+#get dates of account suspensions
+suspdates <- read_csv("data/raw/tracked-suspensions.csv")
+
+#read in follower lists
+suspended_followers_all <- readRDS("data/output/suspended_followers_all.rds")
+
+# take just 3 for testing
+suspended_followers_all <- suspended_followers_all[1:3] #TODO change to all in production
+
+for (i in seq_along(suspended_followers_all)) {
+  
+  #get suspended account ID for file naming
+  suspended_account = colnames(suspended_followers_all[[i]])
+  #make data path for saving
+  data_path_for_follower_tweets = paste0("data/testdata/", suspended_account) #TODO change from testdata folder when in production
+  #get date-time of suspension
+  suspdatetime = suspdates[suspdates$`Twitter ID`== suspended_account,4]
+  suspdate = as.Date(suspdatetime$`Suspension detected`)
+  #get date 30 days before
+  suspdateb = suspdate-30
+  #get date 30 days after
+  suspdatea = suspdate+30
+  #format per Twitter API requirements
+  suspdateb_tformat = paste0(as.character(suspdateb), "T00:00:00Z")
+  suspdatea_tformat = paste0(as.character(suspdatea), "T00:00:00Z")
+  #get followers of suspended account
+  followers = suspended_followers_all[[i]][[suspended_account]]
+  
+  #sample followers
+  set.seed(123L)
+  followers_sample <- sample(followers, 10, replace = F) #TODO change to full sample or sample of max n. in production
+  
+  #get tweets of followers
+  for(j in seq_along(followers_sample)) {
+    
+    #TODO build in error handling here
+    
+    get_all_tweets(
+      users = followers_sample[[j]],
+      start_tweets = suspdateb_tformat,
+      end_tweets = suspdatea_tformat,
+      data_path = data_path_for_follower_tweets,
+      bind_tweets = F,
+      n = Inf
+    )
+    
+  }
+}
